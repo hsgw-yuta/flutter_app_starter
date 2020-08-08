92fc4143cedbfa498bd90f89578d91153f0988c5
deodorantpower <hasegawa.yuta4@gmail.com>
2020-08-08 18:27:59 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutterappstarter/next_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> titleList = ['Amazon', '楽天', 'Yahoo!'];
+
+  // floating onPressed function
+  void _incrementCounter() {
+    setState(() {
+      // Listを追加する
+      titleList.add("追加されたList");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("パスワード管理"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Column(children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.vpn_key),
+              title: Text(titleList[index]),
+              onTap: () {
+                Navigator.push(
+                    // 画面をStack構造で管理する
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            NextPage(titleList[index]) // 遷移先を指定
+                        ));
+              }, // item押下時に画面センチする
+            ),
+            Divider(),
+          ]);
+        },
+        itemCount: titleList.length, // loopCount
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/next_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget {
+  final String title;
+  NextPage(this.title);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        // 縦
+        children: <Widget>[
+          Row(
+            // 横
+            children: <Widget>[
+              Container(
+                width: 50,
+                child: Text('ID'),
+              ),
+              Container(
+                width: 20,
+                child: Text(':'),
+              ),
+              Text('毎回同じテキストを表示ID')
+            ],
+          ),
+
+          // 余白
+          Padding(
+            padding: EdgeInsets.all(5.0),
+          ),
+
+          Row(
+            // 横
+            children: <Widget>[
+              Container(
+                width: 50,
+                child: Text('PW'),
+              ),
+              Container(
+                width: 20,
+                child: Text(':'),
+              ),
+              Text('毎回同じテキストを表示PW')
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
